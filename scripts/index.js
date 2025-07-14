c3ed233db7b21933ae266d68162963178ea4398b
Echoslime <renzocabrera77@gmail.com>
2025-07-13 22:13:25 -0400

    Open and close modal functions

--- scripts/index.js
+++ scripts/index.js
@@ -45,29 +45,37 @@ const addCardFormEl = document.forms["new-post-form"];
 const nameInput = newPostModal.querySelector("#card-caption-input");
 const linkInput = newPostModal.querySelector("#card-image-input");
 
+function openModal(modal){
+    modal.classList.add("modal_is-opened");
+}
+
+function closeModal(modal){
+    modal.classList.remove("modal_is-opened");
+}
+
 editProfileBtn.addEventListener("click", function () {
     editProfileNameInput.value = profileNameEl.textContent;
     editProfileDescriptionInput.value = profileDescriptionEl.textContent;
-    editProfileModal.classList.add("modal_is-opened");
+    openModal(editProfileModal);
 });
 
 editProfileCloseBtn.addEventListener("click", function () {
-    editProfileModal.classList.remove("modal_is-opened");
+    closeModal(editProfileModal);
 });
 
 newPostBtn.addEventListener("click", function () {
-    newPostModal.classList.add("modal_is-opened");
+    openModal(newPostModal);
 });
 
 newPostCloseBtn.addEventListener("click", function () {
-    newPostModal.classList.remove("modal_is-opened");
+    closeModal(newPostModal);
 });
 
 function handleEditProfileSubmit(evt) {
     evt.preventDefault();
     profileNameEl.textContent = editProfileNameInput.value;
     profileDescriptionEl.textContent = editProfileDescriptionInput.value;
-    editProfileModal.classList.remove("modal_is-opened");
+    closeModal(editProfileModal);
 }
 
 editProfileForm.addEventListener("submit", handleEditProfileSubmit);
@@ -76,7 +84,7 @@ function handleAddCardSubmit(evt) {
     evt.preventDefault();
     console.log(nameInput.value);
     console.log(linkInput.value);
-    newPostModal.classList.remove("modal_is-opened");
+    closeModal(newPostModal);
 
     addCardFormEl.reset();
 
